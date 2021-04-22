6cd641c24c82d9b5c4539bbe892571e65e6a90f2
Maggie Nolan <nolanmar@google.com>
2021-04-21 17:20:09 -0700

    chore: test presbumits
    PiperOrigin-RevId: 369768466

--- googlecloudprofiler/client.py
+++ googlecloudprofiler/client.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# test change
 """Communicates with the profiler backend over HTTP."""
 
 import base64
