6996cf6eea8ba814abab5ff625ca5a03b09dbc08
cloud-profiler-team <cloud-profiler-team@google.com>
2022-02-14 09:03:53 -0800

    test: make integration test use Go 1.17.7
    PiperOrigin-RevId: 428525361

--- kokoro/integration_test.sh
+++ kokoro/integration_test.sh
@@ -53,14 +53,23 @@ if [[ "$KOKORO_JOB_TYPE" == "PRESUBMIT_GITHUB" ]]; then
   RUN_BACKOFF_TEST="false"
 fi
 
+# Ensure a newer version of Go is used so it is compatible with newer libraries.
+# Here we install v1.17.7 which is the current version as of when this code
+# was written, following instructions from https://go.dev/doc/manage-install.
+# Go modules might not be on for previous versions of Go, so we also have to
+# enable the module system explicitly.
+export GO111MODULE=on
+go install golang.org/dl/go1.17.7
+go1.17.7 download
+
 # Initializing go modules allows our dependencies to install versions of their
 # dependencies specified by their go.mod files. This reduces the likelihood of
 # dependencies breaking this test.
-go mod init e2e
+go1.17.7 mod init e2e
 
 # Compile test before running to download dependencies.
-retry go get cloud.google.com/go/profiler/proftest@HEAD
-retry go test -c
+retry go1.17.7 get cloud.google.com/go/profiler/proftest@HEAD
+retry go1.17.7 test -c
 ./e2e.test  -gcs_location="${GCS_LOCATION}" -run_backoff_test=$RUN_BACKOFF_TEST
 
 # Exit with success code if no need to release the agent.
